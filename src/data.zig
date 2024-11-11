182b81e15cf06f59b382d725c93c08277674cdae
Taylor McKinney <2374670+taylormck@users.noreply.github.com>
2024-11-12 00:16:46 +0900

    feat: lock the data with a read write lock

--- src/data.zig
+++ src/data.zig
@@ -2,16 +2,26 @@ const std = @import("std");
 const RESP = @import("./resp.zig");
 
 var data: std.StringHashMap([]const u8) = undefined;
+var lock: std.Thread.RwLock = .{};
 
 pub fn initialize_data(allocator: std.mem.Allocator) void {
+    lock.lock();
+    defer lock.unlock();
+
     data = std.StringHashMap([]const u8).init(allocator);
 }
 
 pub fn deinit_data() void {
+    lock.lock();
+    defer lock.unlock();
+
     data.deinit();
 }
 
 pub fn get(key: []const u8) RESP.Value {
+    lock.lockShared();
+    defer lock.unlockShared();
+
     if (data.get(key)) |val| {
         return RESP.Value.bulk_string(val);
     } else {
@@ -20,5 +30,8 @@ pub fn get(key: []const u8) RESP.Value {
 }
 
 pub fn set(key: []const u8, value: []const u8) !void {
+    lock.lock();
+    defer lock.unlock();
+
     try data.put(key, value);
 }
--- test_client.sh
+++ test_client.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# echo -e "PING\nPING" | redis-cli &
-# echo -e "PING\nPING" | redis-cli &
+echo -e "PING\nPING" | redis-cli &
+echo -e "PING\nPING" | redis-cli &
 redis-cli set foo bar
 redis-cli set foo cookie
 redis-cli get foo
 
+echo -e "SET apples 2\nGET apples" | redis-cli &
 redis-cli set foo bar
 redis-cli get foo
